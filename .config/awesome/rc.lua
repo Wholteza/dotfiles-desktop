f758f57b0e280a7e5dc7b446a351f7df01de55d9
Zackarias Montell <zackarias@montell.se>
2021-02-15 18:23:24 +0100

    Automatic commit

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -347,7 +347,14 @@ globalkeys = gears.table.join(
               {description = "lua execute prompt", group = "awesome"}),
     -- Menubar
     awful.key({ modkey }, "p", function() menubar.show() end,
-              {description = "show the menubar", group = "launcher"})
+              {description = "show the menubar", group = "launcher"}),
+    -- Passmenu
+    awful.key(
+        {Mod1, Control},
+        "s", 
+        function() awful.spawn.with_shell("~/.password-store/passmenu") end,
+        {description = "Show the password menu", group = "custom"})
+    
 )
 
 clientkeys = gears.table.join(
