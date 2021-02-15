d438c40d85b25c35f9f338a4e9fbc94bf8a2c6d3
Zackarias Montell <zackarias@montell.se>
2021-02-15 17:58:04 +0100

    Automatic commit

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -582,4 +582,4 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- }}}
 
 -- Autostart Applications
-awful.spawn.with_shell()
+awful.spawn.with_shell("~/.config/wholteza/fehbg/run.sh")
