60088fef0ddf5a8dcb7c96b08d2521970165d0e9
Zackarias Montell <zackarias@montell.se>
2021-02-20 12:27:21 +0100

    Automatic commit

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -327,7 +327,10 @@ globalkeys = gears.table.join(
               {description = "select next", group = "layout"}),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1)                end,
               {description = "select previous", group = "layout"}),
-
+    awful.key({ modkey, "Mod1"    }, "Right",     function () awful.tag.incmwfact( 0.01)    end),
+    awful.key({ modkey, "Mod1"    }, "Left",     function () awful.tag.incmwfact(-0.01)    end),
+    awful.key({ modkey, "Mod1"    }, "Down",     function () awful.client.incwfact( 0.01)    end),
+    awful.key({ modkey, "Mod1"    }, "Up",     function () awful.client.incwfact(-0.01)    end),
     awful.key({ modkey, "Control" }, "n",
               function ()
                   local c = awful.client.restore()
